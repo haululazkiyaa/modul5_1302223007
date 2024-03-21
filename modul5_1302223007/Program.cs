73a812d0cf48619d637ea233b9621c63023d6311
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-03-21 10:44:41 +0700

    menambahkan class SimpleDatabase

--- modul5_1302223007/Program.cs
+++ modul5_1302223007/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SimpleDatabase<String> db = new SimpleDatabase<String>();
+        db.AddNewData("13");
+        db.AddNewData("02");
+        db.AddNewData("22");
+        db.PrintAllData();
+    }
+}
+
+class SimpleDatabase<T>
+{
+    private List<T> storeData;
+    private List<DateTime> inputDates;
+
+    public SimpleDatabase()
+    {
+        this.storeData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        this.storeData.Add(data);
+        this.inputDates.Add(DateTime.Now);
+    }
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storeData.Count; i++)
+        {
+            Console.WriteLine(
+                "Data " + (i+1) + " berisi: " + storeData[i] + ", " +
+                "yang disimpan pada waktu UTC: " + inputDates[i]
+            );
+        }
+    }
+}
